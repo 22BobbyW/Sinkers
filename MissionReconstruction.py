35ab0df82b8ec28c4456687b083844eb8338185d
Matthew Weng <matthew.weng.nyc@gmail.com>
2021-07-22 16:07:53 -0400

    Added new array to store variable

--- MissionReconstruction.py
+++ MissionReconstruction.py
@@ -1,11 +1,13 @@
 import cv2 
 import numpy as np
 import matplotlib.pyplot as plt
-import picamera 
 import time 
 from datetime import datetime 
 import BWSI_Sandshark
 import pandas as pd
+import BWSI_BackSeat
+import csv 
+
 # pictures what the AUV sees vs actual camera
 # AUV's location
 # commands being sent
@@ -17,13 +19,16 @@ class MissionReconstruction():
     def __init__(self):
         self.missionstart = datetime.now()
         self.vehicle = BWSI_Sandshark.Sandshark()
+    
+    def messages(self, status):
+        commands = [][2]
+        n = 0
+        while(True):
+            m = status.split(",")
+            time = m[1]
+            commands[n][0] = time
+            heading = m[9]
+            commands[n][1] = heading 
+            n += 1 
 
-
-    def messages(self, cmd):
-        
-        commands = pd.DataFrame(columns=['time', 'message', 'position'])
-        time = datetime.now()
-
-
-
-
+            
