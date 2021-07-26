87f3133615457f2c8eae77380b516ddff230c102
Bria Erimli <bria.c.erimli@gmail.com>
2021-07-26 14:20:04 -0700

    working mission reconstruction

--- MissionReconstruction.py
+++ MissionReconstruction.py
@@ -1,12 +1,8 @@
-import cv2 
-import numpy as np
-import matplotlib.pyplot as plt
 import time 
 from datetime import datetime
 
 from numpy.lib.utils import info 
 import BWSI_Sandshark
-import pandas as pd
 import csv 
 
 # pictures what the AUV sees vs actual camera
@@ -17,32 +13,75 @@ import csv
 # time count
 
 class Mission_Reconstruction():
+        
     def __init__(self):
         self.missionstart = datetime.now()
         self.vehicle = BWSI_Sandshark.Sandshark()
         self.__index = 0
         self.__time = None
-        self.__commands = np.empty((1,4)) # time, heading, desired heading, speed
+        self.__data = []
 
+            
     # this function is called before store_autonomy_decide 
     # so that they use the same index 
     def messages(self, status):
         m = status.split(",")
-        time = m[1] # change to store time - first time; time elapsed
+        timeStamp = m[1] # change to store time - first time; time elapsed
         heading = m[9]
-
-        self.__commands[self.__index][0] = time
-        self.__commands[self.__index][1] = heading 
-        return
-
+    
+        
+        self.__data.append(timeStamp)
+        self.__data.append(heading)
+        
+        """
+        with open('auvData.csv', 'a') as csvfile:
+            auvData = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            #auvData.writerow(['Time', 'Heading', 'Desired Heading'])
+            
+            auvData.writerow([timeStamp, heading])
+       # return timeStamp, heading
+               """
+      
+    def auv_command(self, cmd):
+        self.__data.append(cmd)
+        
     def store_autonomy_decide(self, information):
+        
         if len(information) == 0:
-            return
+            self.__data.append("no data")
+            self.add_to_csv()
 
-        desired_heading = information[0]
-        speed = information[1]
+            """
+            with open('auvData.csv', 'a') as csvfile:
+                    auvData = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                    #auvData.writerow(['Time', 'Heading', 'Desired Heading'])
+                    
+                    auvData.writerow(["no data"])
+           # return
+           """
+           
+        else:
+            desired_heading = information[0]
+            
+            self.__data.append(desired_heading)
+            self.add_to_csv()
+            """
+            with open('auvData.csv', 'a') as csvfile:
+                auvData = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                #auvData.writerow(['Time', 'Heading', 'Desired Heading'])
+                    
+                auvData.writerow([desired_heading])
 
-        self.__commands[self.__index][2] = desired_heading
-        self.__commands[self.__index][3] = speed
-
-        self.__index += 1
+        """
+            
+        
+     #   self.auvData.writerow([ "", "", desired_heading, speed])
+        
+    def add_to_csv(self):
+        with open('auvData.csv', 'a') as csvfile:
+            auvData = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            #auvData.writerow(['Time', 'Heading', 'Desired Heading'])
+            
+            auvData.writerow(self.__data)
+        self.__data = []
+ 
