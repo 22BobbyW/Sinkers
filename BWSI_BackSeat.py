80b6dbfb0e6726737704e5ce1587e968f0ffe30f
bobbywang <22bobbyw@students.harker.org>
2021-07-29 16:49:15 +0100

    changing from sim to picam

--- BWSI_BackSeat.py
+++ BWSI_BackSeat.py
@@ -55,7 +55,7 @@ class BackSeat():
         self.__datum = None
         
         # set to PICAM for the real camera
-        self.__buoy_detector = ImageProcessor(camera='SIM')
+        self.__buoy_detector = ImageProcessor()
         self.__autonomy = AUVController()
     
     def run(self):
